5e25aad6f495587b1239778444c01f855554e44a
James Aymer <aymerjames@gmail.com>
2024-11-14 16:02:34 +0000

    WIP: first commit

---
+++ contracts/Calculator.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract Calculator {
+
+    uint256 public result = 0;
+
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function getResult() public view returns (uint256) {
+        return result;
+    }
+
+}
